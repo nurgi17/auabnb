41526f80edd5cd2292bccf1d47fd04f9883368fe
Marfa Lamova <invisible.kichi@gmail.com>
2019-10-21 15:28:06 +0600

    SignIn&SignUp pages

---
+++ auabnb/app/src/main/java/n/b/m/auabnb/SignInActivity.java
@@ -0,0 +1,14 @@
+package n.b.m.auabnb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
---
+++ auabnb/app/src/main/java/n/b/m/auabnb/SignUpActivity.java
@@ -0,0 +1,13 @@
+package n.b.m.auabnb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
