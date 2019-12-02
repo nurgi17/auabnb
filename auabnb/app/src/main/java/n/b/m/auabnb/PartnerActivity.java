54660f535bb6c44ac10af48664c68f0e05950bb6
ZhBexultan <zh.beksultan1412@mail.ru>
2019-12-02 16:24:36 +0600

    finished partner pages and added call action

--- auabnb/app/src/main/java/n/b/m/auabnb/PartnerActivity.java
+++ auabnb/app/src/main/java/n/b/m/auabnb/PartnerActivity.java
@@ -3,6 +3,8 @@ package n.b.m.auabnb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +21,17 @@ public class PartnerActivity extends AppCompatActivity {
 
         TextView title = (TextView) findViewById(R.id.helper_title);
         title.setText("Please fill the information");
+
+        Button confirm = (Button) findViewById(R.id.confirm_action);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PartnerAccessActivity.class);
+                v.getContext().startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
